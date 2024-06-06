537396828fb6d1c4990c5e71b33001ef9a7b367e
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-06-07 01:11:45 +0300

    added routes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,32 @@
+import { NavLink, Link } from 'react-router-dom';
+
+import sprite from '../../assets/sprite.svg';
+import clsx from 'clsx';
+import scss from './Header.module.scss';
+
 export const Header = () => {
-  return <div>Header</div>;
+  const navLink = ({ isActive }) => {
+    return clsx(scss.link, isActive && scss.active);
+  };
+  return (
+    <header className={scss.header}>
+      <Link className={scss.wrapperLogo} to="/">
+        <svg className={scss.logo}>
+          <use href={`${sprite}#icon-camper`}></use>
+        </svg>
+        <p className={scss.textLogo}>Camper Rental</p>
+      </Link>
+      <nav className={scss.navWrap}>
+        <NavLink to="/" className={navLink}>
+          Home
+        </NavLink>
+        <NavLink to="/catalog" className={navLink}>
+          Catalog
+        </NavLink>
+        <NavLink to="/favorites" className={navLink}>
+          Favorites
+        </NavLink>
+      </nav>
+    </header>
+  );
 };

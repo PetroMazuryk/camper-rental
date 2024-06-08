fd412175216505841e36a1c72c2d304227bd654d
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-06-09 02:52:25 +0300

    added CamperCard component

--- src/components/CamperCard/CamperCard.jsx
+++ src/components/CamperCard/CamperCard.jsx
@@ -1,11 +1,14 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
 import { Modal } from '../Modal/Modal';
 import { useModal } from '../../components/hooks/useModal';
-import scss from './CamperCard.module.scss';
-import { useSelector } from 'react-redux';
+
 import { selectCampersData } from '../../redux/campers/selectors';
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
 import { fetchCampersAsync } from '../../redux/campers/operations';
+import icon from '../../assets/sprite.svg';
+
+import scss from './CamperCard.module.scss';
 
 export const CamperCard = () => {
   const { isModalOpen, openModal, closeModal } = useModal();
@@ -18,13 +21,93 @@ export const CamperCard = () => {
   }, [dispatch]);
 
   return (
-    <div>
-      <button className={scss.button} type="button" onClick={openModal}>
-        Show more
-      </button>
-      <Modal isOpen={isModalOpen} onClose={closeModal}>
-        <div>Camper Card</div>
-      </Modal>
-    </div>
+    <>
+      {campers.length > 0 && (
+        <ul className={scss.list}>
+          {campers.map(
+            ({
+              _id,
+              name,
+              // location,
+              price,
+              gallery,
+              description,
+              // rating,
+              // reviews,
+            }) => {
+              return (
+                <li className={scss.item} key={_id}>
+                  <div className={scss.imageWrapper}>
+                    <img className={scss.image} src={gallery[0]} alt={name} />
+                  </div>
+
+                  <div className={scss.infoWrapper}>
+                    <div className={scss.headingInner}>
+                      <h3 className={scss.heading}>{name}</h3>
+                      <div className={scss.priceInner}>
+                        <p className={scss.heading}>
+                          &#8364;{price.toFixed(2)}
+                        </p>
+                        <button className={scss.buttonFavourite}>
+                          <svg className={scss.active} width="21" height="19">
+                            <use href={`${icon}#icon-favourite`}></use>
+                          </svg>
+                        </button>
+                      </div>
+                    </div>
+
+                    <div className={scss.boxInner}>
+                      <div className={scss.subjectInner}>
+                        <div className={scss.holder}>
+                          <svg
+                            className={scss.iconRating}
+                            width="16"
+                            height="16"
+                          >
+                            <use href={`${icon}#icon-rating`}></use>
+                          </svg>
+                          <p className={`${scss.subject} ${scss.reviews}`}>
+                            <div>Reviews info</div>
+                          </p>
+                        </div>
+
+                        <div className={scss.holder}>
+                          <svg
+                            className={scss.iconLocation}
+                            width="16"
+                            height="16"
+                          >
+                            <use href={`${icon}#icon-location`}></use>
+                          </svg>
+                          <p className={scss.subject}>
+                            <div>Location</div>
+                          </p>
+                        </div>
+                      </div>
+
+                      <p className={scss.desc}>{description}</p>
+                      <ul className={scss.list}>
+                        <div>FeaturesCategories</div>
+                      </ul>
+                      <button
+                        className={scss.button}
+                        type="button"
+                        onClick={openModal}
+                      >
+                        Show more
+                      </button>
+
+                      <Modal isOpen={isModalOpen} onClose={closeModal}>
+                        <div>Camper Card</div>
+                      </Modal>
+                    </div>
+                  </div>
+                </li>
+              );
+            }
+          )}
+        </ul>
+      )}
+    </>
   );
 };
--- src/services/apiCamper.js
+++ src/services/apiCamper.js
@@ -4,18 +4,18 @@ export const apiInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
-// export const campersFetch = async (page = 1) => {
-//   const { data } = await instance.get('/adverts', {
-//     params: {
-//       page,
-//       limit: 4,
-//     },
-//   });
+export const campersFetch = async (page = 1) => {
+  const { data } = await apiInstance.get('/adverts', {
+    params: {
+      page,
+      limit: 4,
+    },
+  });
 
-//   return data;
-// };
-
-export const campersFetch = async () => {
-  const { data } = await apiInstance.get('/adverts');
   return data;
 };
+
+// export const campersFetch = async () => {
+//   const { data } = await apiInstance.get('/adverts');
+//   return data;
+// };

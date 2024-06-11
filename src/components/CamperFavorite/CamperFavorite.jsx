76679c60845858a23da707556cc8713f55e81765
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-06-11 10:59:51 +0300

    added the code to the favorite page

---
+++ src/components/CamperFavorite/CamperFavorite.jsx
@@ -0,0 +1,20 @@
+import { useSelector } from 'react-redux';
+import { CamperItem } from '../CamperCard/CamperItem/CamperItem';
+import { selectFavoriteCampers } from '../../redux/favorite/selectors';
+import scss from './CamperFavorite.module.scss';
+
+export const CamperFavorite = () => {
+  const favoriteCampers = useSelector(selectFavoriteCampers);
+
+  return (
+    <div className={scss.camperWrapper}>
+      <ul className={scss.listWrapper}>
+        {favoriteCampers.map(item => (
+          <li className={scss.item} key={item._id}>
+            <CamperItem item={item} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,5 +1,12 @@
+import { CamperFavorite } from '../../components/CamperFavorite/CamperFavorite';
+import scss from './Favorites.module.scss';
+
 const Favorites = () => {
-  return <div>Favorites</div>;
+  return (
+    <div className={scss.favoriteWrapper}>
+      <CamperFavorite />
+    </div>
+  );
 };
 
 export default Favorites;

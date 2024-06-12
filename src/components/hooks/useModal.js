52d3a96d9c925984b01bb06c8074d7ce443b2fb9
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-06-12 16:41:57 +0300

    fixed background scrolling

--- src/components/hooks/useModal.js
+++ src/components/hooks/useModal.js
@@ -2,8 +2,14 @@ import { useState } from 'react';
 
 export const useModal = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const openModal = () => setIsModalOpen(true);
-  const closeModal = () => setIsModalOpen(false);
+  const openModal = () => {
+    setIsModalOpen(true);
+    document.body.style.overflow = 'hidden';
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+    document.body.style.overflow = 'auto';
+  };
 
   return { isModalOpen, openModal, closeModal };
 };

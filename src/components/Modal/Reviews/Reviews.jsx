a8de781e451e7e368da3a2e2763c0f2f5d8ab7df
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-06-14 15:56:58 +0300

    changed the color rendering of the stars according to the number

--- src/components/Modal/Reviews/Reviews.jsx
+++ src/components/Modal/Reviews/Reviews.jsx
@@ -5,16 +5,22 @@ import scss from './Reviews.module.scss';
 export const Reviews = ({ item }) => {
   const { reviews } = item;
 
+  const renderStars = reviewer_rating => {
+    const stars = [];
+    for (let i = 0; i < 5; i++) {
+      const starClass = i < reviewer_rating ? scss.yellow : scss.white;
+      stars.push(
+        <svg key={i} className={`${scss.starIcon} ${starClass}`}>
+          <use href={`${icons}#icon-rating`}></use>
+        </svg>
+      );
+    }
+    return stars;
+  };
+
   const elements = reviews.map(
     ({ reviewer_name, reviewer_rating, comment }, index) => {
-      const rating = [];
-      for (let i = 0; i < reviewer_rating; i++) {
-        rating.push(
-          <svg key={i} className={scss.starIcon}>
-            <use href={`${icons}#icon-rating`}></use>
-          </svg>
-        );
-      }
+      const stars = renderStars(reviewer_rating);
 
       return (
         <li key={index} className={scss.item}>
@@ -24,7 +30,7 @@ export const Reviews = ({ item }) => {
             </div>
             <div>
               <h3 className={scss.author}>{reviewer_name}</h3>
-              {rating}
+              <div className={scss.stars}>{stars}</div>
             </div>
           </div>
           <p className={scss.comment}>{comment}</p>

69bef5bddf4453828b6203b0926c74376d34230b
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2025-07-30 18:00:24 +0300

    fix styled components

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,13 +3,12 @@ import { Outlet } from 'react-router-dom';
 
 import { Header } from '../Header/Header';
 import { ToasterBar } from '../ToasterBar';
+import { Container } from '../Container/Container';
 import { Loader } from '../../components/Loader/Loader';
-
-import scss from './Layout.module.scss';
 
 export const Layout = () => {
   return (
-    <div className={scss.wrapper}>
+    <Container>
       <Header />
       <main>
         <div>
@@ -19,6 +18,6 @@ export const Layout = () => {
           <ToasterBar />
         </div>
       </main>
-    </div>
+    </Container>
   );
 };
--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,14 +1,12 @@
 import { CamperFavorite } from '../../components/CamperFavorite/CamperFavorite';
-import { Container } from '../../components/Container/Container';
+// import { Container } from '../../components/Container/Container';
 import scss from './Favorites.module.scss';
 
 const Favorites = () => {
   return (
-    <Container>
-      <div className={scss.favoriteWrapper}>
-        <CamperFavorite />
-      </div>
-    </Container>
+    <div className={scss.favoriteWrapper}>
+      <CamperFavorite />
+    </div>
   );
 };
 

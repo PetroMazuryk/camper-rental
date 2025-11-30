1d026688f23ff1857804900eb55828508f2692ab
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2025-11-30 23:51:02 +0200

    update Layout to use LoaderSmall for route lazy-loading

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,7 @@
 import { Suspense } from 'react';
+import { useSelector } from 'react-redux';
 import { Outlet } from 'react-router-dom';
+import { selectIsCampersLoading } from '../../redux/campers/selectors';
 
 import { Header } from '../Header/Header';
 import { ToasterBar } from '../ToasterBar';
@@ -7,9 +9,11 @@ import { Container } from '../Container/Container';
 import { Loader } from '../../components/Loader/Loader';
 
 export const Layout = () => {
+  const isCampersLoading = useSelector(selectIsCampersLoading);
   return (
     <Container>
       <Header />
+      {isCampersLoading && <Loader message="Data is loading, please wait..." />}
       <main>
         <div>
           <Suspense fallback={<Loader />}>
--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,10 +1,12 @@
 import { SiReactos } from 'react-icons/si';
 import scss from './Loader.module.scss';
 
-export const Loader = () => {
+export const Loader = ({ message }) => {
   return (
     <div className={scss.wrapper}>
       <SiReactos className={scss.loader} size={50} color="aqua" />
+
+      {message && <p className={scss.text}>{message}</p>}
     </div>
   );
 };
